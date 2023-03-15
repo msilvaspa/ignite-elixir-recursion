20209f1bc11903e9da56cfea75bf3a8a24046c6f
Mateus Alves <mateus@Mateuss-MacBook-Pro.local>
2023-03-15 15:16:53 -0300

    challenge done

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    count(list, 0)
+  end
+
+  defp count([], qtd), do: qtd
+
+  defp count([head | tail], qtd) do
+    count(tail, qtd + 1)
   end
 end
